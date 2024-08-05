c37c5bd9e9395c0301ad7d1adfe91ef81670dabd
Sanjeev Midda <110365686+SanjeevMidda@users.noreply.github.com>
2024-08-05 12:33:14 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,68 +14,66 @@ function App() {
 
   return (
     <div className="App">
-      <div className="appContainer">
-        <header>
-          <Link
-            color="blue"
-            text="vowels"
-            changeText={setSelected}
-            changeColor={setMainCircleColor}
-          />
-          <Link
-            color="pink"
-            text="consonants"
-            changeText={setSelected}
-            changeColor={setMainCircleColor}
-          />
-          <Link
-            color="purple"
-            text="verbs"
-            changeText={setSelected}
-            changeColor={setMainCircleColor}
-          />
-          <Link
-            color="orange"
-            text="nouns"
-            changeText={setSelected}
-            changeColor={setMainCircleColor}
-          />
-          <Link
-            color="green"
-            text="room"
-            changeText={setSelected}
-            changeColor={setMainCircleColor}
-          />
-        </header>
+      <header>
+        <Link
+          color="blue"
+          text="vowels"
+          changeText={setSelected}
+          changeColor={setMainCircleColor}
+        />
+        <Link
+          color="pink"
+          text="consonants"
+          changeText={setSelected}
+          changeColor={setMainCircleColor}
+        />
+        <Link
+          color="purple"
+          text="verbs"
+          changeText={setSelected}
+          changeColor={setMainCircleColor}
+        />
+        <Link
+          color="orange"
+          text="nouns"
+          changeText={setSelected}
+          changeColor={setMainCircleColor}
+        />
+        <Link
+          color="green"
+          text="room"
+          changeText={setSelected}
+          changeColor={setMainCircleColor}
+        />
+      </header>
 
-        <div className="mainContainer">
-          {selected === "vowels" && (
-            <Vowels changeLanguage={changeLanguage}></Vowels>
-          )}
-          {selected === "consonants" && (
-            <Consonants changeLanguage={changeLanguage}></Consonants>
-          )}
-          {selected === "verbs" && (
-            <Verbs changeLanguage={changeLanguage}></Verbs>
-          )}
-          {selected === "nouns" && (
-            <Nouns changeLanguage={changeLanguage}></Nouns>
-          )}
-          {selected === "room" && <Room changeLanguage={changeLanguage}></Room>}
-        </div>
-
-        <footer>
-          {/* <h1>{selected}</h1> */}
-          <h3 onClick={() => setChangeLanguage(!changeLanguage)}>
-            {changeLanguage ? "Korean" : "English"}
-          </h3>
-          <div
-            className="mainCircle"
-            style={{ backgroundColor: mainCircleColor }}
-            onClick={() => setChangeLanguage(!changeLanguage)}
-          ></div>
-        </footer>
+      <div className="mainContainer">
+        {selected === "vowels" && (
+          <Vowels changeLanguage={changeLanguage}></Vowels>
+        )}
+        {selected === "consonants" && (
+          <Consonants changeLanguage={changeLanguage}></Consonants>
+        )}
+        {selected === "verbs" && (
+          <Verbs changeLanguage={changeLanguage}></Verbs>
+        )}
+        {selected === "nouns" && (
+          <Nouns changeLanguage={changeLanguage}></Nouns>
+        )}
+        {selected === "room" && <Room changeLanguage={changeLanguage}></Room>}
       </div>
+
+      <footer>
+        {/* <h1>{selected}</h1> */}
+        <h3 onClick={() => setChangeLanguage(!changeLanguage)}>
+          {changeLanguage ? "Korean" : "English"}
+        </h3>
+        <div
+          className="mainCircle"
+          style={{ backgroundColor: mainCircleColor }}
+          onClick={() => setChangeLanguage(!changeLanguage)}
+        ></div>
+      </footer>
     </div>
   );
 }
